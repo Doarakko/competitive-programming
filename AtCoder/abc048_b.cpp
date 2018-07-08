da7c7629f6b3209e5c8c4d68599c0152d6f51e27
Doarakko <hideo16omo@gmail.com>
2018-07-08 16:54:43 +0900

    tommorow is monday...

--- AtCoder/abc048_b.cpp
+++ AtCoder/abc048_b.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 using ll = long long;
 
 int main(int argc, const char * argv[]) {
-    ll a, b, x;
+    ll a, b, x, cnt;
     cin >> a >> b >> x;
-    if (a == b || a < x) {
-        cout << b / x - a / x << endl;
-    }else{
-        cout << b / x - a / x + 1 << endl;
+    cnt = b / x - a / x;
+    if (a % x == 0) {
+        cnt++;
     }
-    
+    cout << cnt << endl;
     return 0;
 }
---
+++ AtCoder/abc048_b/abc048_b/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  abc048_b
+//
+//  Created by Yuuki Tanaami on 2018/07/08.
+//  Copyright © 2018年 Yuuki Tanaami. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
