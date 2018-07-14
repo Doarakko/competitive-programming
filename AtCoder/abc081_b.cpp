5783e794eeaadb3d2c3417c722d32d9fb43eca09
Doarakko <hideo16omo@gmail.com>
2018-07-14 11:10:40 +0900

    3rennkyu!

--- AtCoder/abc081_b.cpp
+++ AtCoder/abc081_b.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 using ll = long long;
 
 int main(int argc, const char * argv[]) {
-    int N;
-    cin >> N;
-    
-    ll a;
-    vector<ll> v(N);
-    bool flag = true;
-    int min = 100;
-    for (int i = 0; i < N; i++) {
-        cin >> a;
-        if(a % 2 == 1){
-            flag = false;
+    int n, cnt, min = 1000000000;
+    cin >> n;
+    ll x;
+    //vector<ll> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        //v[i] = x;
+        cnt = 0;
+        while (x % 2 == 0) {
+            x /= 2;
+            cnt++;
         }
-        v[i] = a;
-    }
-    
-    if(flag){
-        for (int i = 0; i < N; i++) {
-            int cnt = 0;
-            while(v[i] % 2 == 0){
-                v[i] = v[i] / 2;
-                cnt++;
-            }
-            if(cnt < min){
-                min = cnt;
-            }
+        if (cnt < min) {
+            min = cnt;
         }
-    }else{
-        min = 0;
     }
     cout << min << endl;
     
