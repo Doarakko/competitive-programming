923336f32c415e092e72c300035ebe50f37b65e6
Doarakko <hideo16omo@gmail.com>
2018-07-18 00:09:16 +0900

    summer

--- AtCoder/abc077_b.cpp
+++ AtCoder/abc077_b.cpp
@@ -8,14 +8,8 @@ using namespace std;
 using ll = long long;
 
 int main(int argc, const char * argv[]) {
-    string s;
-    cin >> s;
-    int cnt = 0;
-    for (int i = 0; i < 3; i++) {
-        if (s[i] == '1') {
-            cnt++;
-        }
-    }
-    cout << cnt << endl;
+    ll n;
+    cin >> n;
+    cout << (int)sqrt(n) * (int)sqrt(n) << endl;
     return 0;
 }
