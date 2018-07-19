29326f269bef7d31a36486d147b5580624ea93a6
Doarakko <2wua4nlyi@gmail.com>
2018-07-19 23:45:03 +0900

    jingu

---
+++ AtCoder/abc003_a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    double sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        sum += i * 10000.0 / n;
+    }
+    cout << (int)sum << endl;
+    return 0;
+}
