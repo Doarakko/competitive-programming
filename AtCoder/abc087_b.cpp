6e2f07e6047ed2d786ee35429149b553a2e5dc25
Doarakko <2wua4nlyi@gmail.com>
2018-07-23 21:14:27 +0900

    yeah

--- AtCoder/abc087_b.cpp
+++ AtCoder/abc087_b.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+#define F first
+#define S second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
 using namespace std;
 using ll = long long;
 
 int main(int argc, const char * argv[]) {
-    int A, B, C, X;
-    cin >> A >> B >> C >> X;
-    
-    int n500, n100, n50, a[3] = {}, sum = 0, cnt = 0;
-    n500 = X / 500;
-    n100 = X / 100;
-    n50 = X / 50;
-    for (int i = 0; i <= n500 && i <= A; i++) {
-        a[0] = (i * 500);
-        for (int j = 0; j <= n100 && j <= B; j++) {
-            a[1] = (j * 100);
-            for (int k = 0; k <= n50 && k <= C; k++) {
-                a[2] = (k * 50);
-                sum = a[0] + a[1] + a[2];
-                if (sum == X) {
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+    int sum, cnt = 0;
+    for (int i = 0; i <= a; i++) {
+        for (int j = 0; j <= b; j++) {
+            for (int k = 0; k <= c; k++) {
+                sum = 500 * i + 100 * j + 50 * k;
+                if (sum == x) {
                     cnt++;
+                    break;
                 }
             }
         }
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Competitive-Programming<br>
-
-・AOJ<br>
-　http://judge.u-aizu.ac.jp/onlinejudge/<br>
-・AtCoder<br>
-　http://www.atcoder.jp/<br>
-・ICPC<br>
-　http://icpc.iisf.or.jp/<br>
-・JOI<br>
-　https://www.ioi-jp.org/<br>
+##・ant-book
+　プログラミングコンテストチャレンジブック
+##・AOJ
+　http://judge.u-aizu.ac.jp/onlinejudge/
+##・AtCoder<br>
+　http://www.atcoder.jp/
+##・ICPC
+　http://icpc.iisf.or.jp/
+##・JOI
+　https://www.ioi-jp.org/
