6ad9aac7a3f681f42c98c2df55c99703e3ad5e39
Doarakko <2wua4nlyi@gmail.com>
2018-07-26 23:50:11 +0900

    yeah yeah yeah

---
+++ AtCoder/abc076_a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+#define F first
+#define S second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    int r, g;
+    cin >> r >> g;
+    cout << 2 * g - r  << endl;
+    return 0;
+}
