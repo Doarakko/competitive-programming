fd7f196ebb4e594ac2bf0854514fb1a0b384ecf2
Doarakko <2wua4nlyi@gmail.com>
2018-08-01 22:35:41 +0900

    hi

---
+++ AtCoder/abc070_a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+#define F first
+#define S second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    string x, y;
+    cin >> x;
+    y = x;
+    vrev(x);
+    if (x == y) {
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
