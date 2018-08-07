99c97bf5f75e7f1fd8c378b1b1780e1f81a85582
Doarakko <2wua4nlyi@gmail.com>
2018-08-07 21:11:15 +0900

    yeah

--- AtCoder/abc057_a.cpp
+++ AtCoder/abc057_a.cpp
@@ -4,26 +4,22 @@
 #include <map>
 #include <algorithm>
 #include <cmath>
-#include <climits>
 
 #define F first
 #define S second
 #define vsort(v) sort((v).begin(), (v).end())
 #define vrev(v) reverse((v).begin(), (v).end())
 
-
-//INT_MAX
-//INT_MIN
-//LLONG_MAX
-//LLONG_MIN
-//DBL_MIN
-//DBL_MAX
-//LDBL_MIN
-//LDBL_MAX
-
 using namespace std;
 using ll = long long;
 
 int main(int argc, const char * argv[]) {
-
+    int a, b;
+    cin >> a >> b;
+    if (a + b < 24) {
+        cout << a + b << endl;
+    }else{
+        cout << a + b - 24 << endl;
+    }
+    return 0;
 }
