309836b4185d5322f3e46af228a6e3240470d375
Doarakko <2wua4nlyi@gmail.com>
2018-09-02 12:54:03 +0900

    add abc107 & 108

---
+++ AtCoder/abc108_a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+#define X first
+#define Y second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+
+//INT_MAX
+//INT_MIN
+//LLONG_MAX
+//LLONG_MIN
+//DBL_MIN
+//DBL_MAX
+//LDBL_MIN
+//LDBL_MAX
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    int k;
+    cin >> k;
+    cout << (k / 2) * (k - k / 2) << endl;
+}
