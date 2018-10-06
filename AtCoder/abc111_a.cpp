cb0b0b92cf734d7015ca9fa453f18b7b5283fad0
Doarakko <2wua4nlyi@gmail.com>
2018-10-07 04:21:32 +0900

    tired

---
+++ AtCoder/abc111_a.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+#define X first
+#define Y second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+
+//INT_MAX
+//INT_MIN
+//LLONG_MAX
+//LLONG_MIN
+//DBL_MIN
+//DBL_MAX
+//LDBL_MIN
+//LDBL_MAX
+
+// A-Z: 65~90
+// a-z: 97~122
+// |a-z| = 26
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '1') {
+            cout << '9';
+        }else if(s[i] == '9'){
+            cout << '1';
+        }
+    }
+    cout << endl;
+}
---
+++ AtCoder/abc112_b.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+#define X first
+#define Y second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+
+//INT_MAX
+//INT_MIN
+//LLONG_MAX
+//LLONG_MIN
+//DBL_MIN
+//DBL_MAX
+//LDBL_MIN
+//LDBL_MAX
+
+// A-Z: 65~90
+// a-z: 97~122
+// |a-z| = 26
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    int n, t;
+    cin >> n >> t;
+    int ans = 10000;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        if (y <= t && ans > x) {
+            ans = x;
+        }
+    }
+    if (ans == 10000) {
+        cout << "TLE" << endl;
+    }else{
+        cout << ans << endl;
+    }
+    
+    
+}
