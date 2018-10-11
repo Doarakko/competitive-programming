db99cd7bc5be79e69d9481e9345318e5cf95a52c
Doarakko <2wua4nlyi@gmail.com>
2018-10-11 23:32:19 +0900

    add abc040

---
+++ AtCoder/abc040_b.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+#define X first
+#define Y second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+
+//INT_MAX
+//INT_MIN
+//LLONG_MAX
+//LLONG_MIN
+//DBL_MIN
+//DBL_MAX
+//LDBL_MIN
+//LDBL_MAX
+
+// A-Z: 65~90
+// a-z: 97~122
+// |a-z| = 26
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    int min = 1000000;
+    for (int i = 1; i <= n / 2 + 1; i++) {
+        for (int j = 1; j <= n / 2 + 1; j++) {
+            if (i * j > n) {
+                break;
+            }else if (min > abs(i - j) + n - (i * j)){
+                min = abs(i - j) + n - (i * j);
+            }
+        }
+    }
+    cout << min << endl;
+    
+}
