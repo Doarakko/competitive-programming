faad4c5efaeebb4d91e7b93ebea0bd31a1d25001
Doarakko <2wua4nlyi@gmail.com>
2018-10-12 23:35:11 +0900

    add 039

---
+++ AtCoder/abc039_a.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+#define X first
+#define Y second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+
+//INT_MAX
+//INT_MIN
+//LLONG_MAX
+//LLONG_MIN
+//DBL_MIN
+//DBL_MAX
+//LDBL_MIN
+//LDBL_MAX
+
+// A-Z: 65~90
+// a-z: 97~122
+// |a-z| = 26
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a * b * 2 + a * c * 2 + b * c * 2 << endl;
+}
