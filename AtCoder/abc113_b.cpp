13be160d286be956a49f6e9bc598cc72763434a4
Doarakko <2wua4nlyi@gmail.com>
2018-12-28 22:47:02 +0900

    💔

--- AtCoder/abc113_b.cpp
+++ AtCoder/abc113_b.cpp
@@ -12,7 +12,6 @@
 #define vsort(v) sort((v).begin(), (v).end())
 #define vrev(v) reverse((v).begin(), (v).end())
 
-
 // INT_MAX
 // INT_MIN
 // LLONG_MAX
@@ -29,6 +28,21 @@
 using namespace std;
 using ll = long long;
 
-int main(int argc, const char * argv[]) {
-
+int main(int argc, const char *argv[]){
+    int n, t, a, h;
+    cin >> n >> t >> a;
+    int ans = -1;
+    double dis = -1;
+    for (int i = 0; i < n; i++){
+        cin >> h;
+        if (i == 0){
+            ans = 1;
+            dis = abs(a - (t - h * 0.006));
+        }
+        else if (dis > abs(a - (t - h * 0.006))){
+            ans = i + 1;
+            dis = abs(a - (t - h * 0.006));
+        }
+    }
+    cout << ans << endl;
 }
--- AtCoder/abc113_d.cpp
+++ AtCoder/abc113_d.cpp
@@ -12,7 +12,6 @@
 #define vsort(v) sort((v).begin(), (v).end())
 #define vrev(v) reverse((v).begin(), (v).end())
 
-
 // INT_MAX
 // INT_MIN
 // LLONG_MAX
@@ -29,6 +28,6 @@
 using namespace std;
 using ll = long long;
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]){
 
 }
