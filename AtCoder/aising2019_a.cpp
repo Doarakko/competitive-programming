6e8671fe4d773c5455cb0df28b521ccb862e4a4f
Doarakko <2wua4nlyi@gmail.com>
2019-01-14 19:36:55 +0900

    😭

---
+++ AtCoder/aising2019_a.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+#define X first
+#define Y second
+#define vsort(v) sort((v).begin(), (v).end())
+#define vrev(v) reverse((v).begin(), (v).end())
+
+// stoi(s): string to int
+// stod(s): string to double
+
+// INT_MAX
+// INT_MIN
+// LLONG_MAX
+// LLONG_MIN
+// DBL_MIN
+// DBL_MAX
+// LDBL_MIN
+// LDBL_MAX
+
+// A-Z: 65~90
+// a-z: 97~122
+// |a-z| = 26
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, const char * argv[]) {
+    int n, h, w;
+    cin >> n >> h >> w;
+    
+    cout << (n - h + 1) * (n - w + 1) << endl;
+}
