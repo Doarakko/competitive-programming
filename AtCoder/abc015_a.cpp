2d517ec21809d3ba7e0495f0ce02d088be4ad8e8
Doarakko <2wua4nlyi@gmail.com>
2019-02-15 23:12:10 +0900

    yameru

---
+++ AtCoder/abc015_a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    string a, b;
+    cin >> a >> b;
+    if (a.length() < b.length()) {
+        cout << b << endl;
+    }else{
+        cout << a << endl;
+    }
+}
