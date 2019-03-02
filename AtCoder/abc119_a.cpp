49e5052f38e89b24f03c2abbfb856fab03c7237f
Doarakko <2wua4nlyi@gmail.com>
2019-03-03 07:17:57 +0900

    ye

---
+++ AtCoder/abc119_a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    string s;
+    cin >> s;
+    if (s <= "2019/04/30") {
+        cout << "Heisei" << endl;
+    }else{
+        cout << "TBD" << endl;
+    }
+}
