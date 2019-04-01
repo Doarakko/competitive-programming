8fc76b9b2297ee9aadfca8886bda5b83ae0186a9
Doarakko <2wua4nlyi@gmail.com>
2019-04-01 14:17:43 +0900

    i==0のとき注意

--- AtCoder/abc024_b.cpp
+++ AtCoder/abc024_b.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     int n, t;
-    
-    
-    
+    cin >> n >> t;
+
+    int x, sum = 0, pre = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        if (i == 0 || x >= pre + t)
+        {
+            sum += t;
+        }
+        else
+        {
+            sum += x - pre;
+        }
+        pre = x;
+        // cout << "x: " << x << " sum: " << sum << endl;
+    }
+    cout << sum << endl;
 }
