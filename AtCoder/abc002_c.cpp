78df7255fa1778649f3e7ae6f7c6d57c9bcbe7a5
Doarakko <2wua4nlyi@gmail.com>
2019-04-04 22:12:56 +0900

    へロンの公式（平行移動もあるよ

---
+++ AtCoder/abc002_c.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+int main(int argc, const char *argv[])
+{
+    vector<double> x(3), y(3);
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> x[i] >> y[i];
+    }
+    // へロンの公式
+    double S, s, a, b, c;
+    a = sqrt((x[0] - x[1]) * (x[0] - x[1]) + (y[0] - y[1]) * (y[0] - y[1]));
+    b = sqrt((x[0] - x[2]) * (x[0] - x[2]) + (y[0] - y[2]) * (y[0] - y[2]));
+    c = sqrt((x[1] - x[2]) * (x[1] - x[2]) + (y[1] - y[2]) * (y[1] - y[2]));
+    s = (a + b + c) / 2;
+    S = sqrt(s * (s - a) * (s - b) * (s - c));
+    //cout << "a: " << a << " b: " << b << " c: " << c << " s: " << s << endl;
+    printf("%.1f", S);
+}
