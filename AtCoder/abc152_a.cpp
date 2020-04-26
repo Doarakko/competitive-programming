22103d145016233561843a06a25e0bdee068909b
Doarakko <2wua4nlyi@gmail.com>
2020-04-27 03:24:08 +0900

    fix a

--- AtCoder/abc152_a.cpp
+++ AtCoder/abc152_a.cpp
@@ -4,30 +4,14 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-    string a, b;
-    cin >> a >> b;
-
-    int x, y;
-    x = stoi(a);
-    y = stoi(b);
-
-    string s, t;
-    for (int i = 0; i < y; i++)
+    int n, m;
+    cin >> n >> m;
+    if (n == m)
     {
-        s += a;
-    }
-
-    for (int i = 0; i < x; i++)
-    {
-        t += b;
-    }
-
-    if (s <= t)
-    {
-        cout << s << endl;
+        cout << "Yes" << endl;
     }
     else
     {
-        cout << t << endl;
+        cout << "No" << endl;
     }
 }
